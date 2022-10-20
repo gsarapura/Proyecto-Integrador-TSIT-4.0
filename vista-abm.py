e30ea999d1cc8e1335c39f66d370fd52486848b6
Gustavs <gustavo.sarapura7@gmail.com>
2022-10-20 19:17:04 -0300

    Agregar vista-abm.py

---
+++ vista-abm.py
@@ -0,0 +1,25 @@
+import controlador
+
+while True:
+    print("\n+-------------------------------------------+")
+    print("|         DISQUERÍA FORMOSA MUSICAL         |")
+    print("+-------------------------------------------+\n")
+    print("")
+    print("MENÚ ABM\n")
+    print("1 - ALTA DE UN ÁLBUM")
+    print("2 - BAJA DE UN ÁBUM")
+    print("3 - MODIFICACIÓN DE UN ÁLBUM")
+    print("4 - SALIR")
+    print("\n")
+    opcion = int(input("Ingrese su opción: "))
+
+    if opcion == 1:
+        None
+    elif opcion == 2:
+        None
+    elif opcion == 3:
+        None
+    elif opcion == 4:
+        break
+    else:
+        print("¡Opción incorrecta!")

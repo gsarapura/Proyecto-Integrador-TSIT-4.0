5a2a780c9b9314d389485d1f6387b72538d75f53
Gustavs <gustavo.sarapura7@gmail.com>
2022-10-22 20:09:42 -0300

    Agregar modelo.py base

--- modelo.py
+++ modelo.py
@@ -1,7 +1,3 @@
-# Acomodar los métodos Listar
-# Colocar if self.conexion.is_connected(): dentro de try - except?
-# Por qué comentar los #self.conexion.close():?
-# Métodos insertar # Vengan como objeto?
 import mysql.connector
 
 class Conectar():
@@ -12,24 +8,26 @@ class Conectar():
                 host = 'localhost',
                 port = 3306,
                 user = 'root',
-                password = '1234',
+                password = '',
                 db = 'disqueria'
+
             )
         except mysql.connector.Error as descripcionError:
             print("¡No se conectó!",descripcionError)
-# Como está.    
+    
     def ListarAlbumes(self):
-        if self.conexion.is_connected(): # Duda
+        if self.conexion.is_connected():
             try:
                 cursor = self.conexion.cursor()
-                senteciaSQL = "SELECT cod_album, nombre_album, nombre_interprete, apellido_apellido, nombre_gen, nombre_disc, precio, cantidad, formato.tipo FROM album, interprete, discografica,formato,genero WHERE album.id_interprete = interprete.id_interprete AND album.id_discografica = discografica.id_discografica AND album.id_formato = formato.id_formato AND album.id_genero = genero.id_genero ORDER By interprete.apellido desc"
+                senteciaSQL = "SELECT cod_album, album.nombre, interprete.nombre, interprete.apellido, genero.nombre, discografica.nombre, precio, cantidad, formato.tipo FROM album, interprete, discografica,formato,genero WHERE album.id_interprete = interprete.id_interprete AND album.id_discografica = discografica.id_discografica AND album.id_formato = formato.id_formato AND album.id_genero = genero.id_genero ORDER By interprete.apellido desc"
                 cursor.execute(senteciaSQL)
                 resultados = cursor.fetchall()
                 self.conexion.close()
                 return resultados
-            except mysql.connector.Error as descripcionError:
-                print("¡No se conectó!",descripcionError) # Duda poner el error afuera?
 
+
+            except mysql.connector.Error as descripcionError:
+                print("¡No se conectó!",descripcionError)
     def ListarPorGenero(self):
         if self.conexion.is_connected():
             try:
@@ -37,8 +35,10 @@ class Conectar():
                 senteciaSQL = "SELECT cod_album, album.nombre, interprete.nombre, interprete.apellido, genero.nombre, discografica.nombre, precio, cantidad, formato.tipo FROM album, interprete, discografica,formato,genero WHERE album.id_interprete = interprete.id_interprete AND album.id_discografica = discografica.id_discografica AND album.id_formato = formato.id_formato AND album.id_genero = genero.id_genero ORDER By genero.nombre asc"
                 cursor.execute(senteciaSQL)
                 resultados = cursor.fetchall()
-                 # why dis
+                #self.conexion.close()
                 return resultados
+
+
             except mysql.connector.Error as descripcionError:
                 print("¡No se conectó!",descripcionError)
 
@@ -55,7 +55,7 @@ class Conectar():
             except mysql.connector.Error as descripcionError:
                 print("¡No se conectó!",descripcionError)
 
-    def InsertarInterprete(self,nombre,apellido,nacionalidad,foto): 
+    def InsertarInterprete(self,nombre,apellido,nacionalidad,foto):
         if self.conexion.is_connected():
             try:
                 cursor = self.conexion.cursor()
@@ -167,216 +167,180 @@ class Conectar():
 #------------------------------------------------------------------------------------------------------
 class Interprete():     
 
-    def __init__(self, id_interprete, nombre_interprete, apellido_interprete, nacionalidad, foto) -> None:
+    def __init__(self,id_interprete,nombre,apellido,nacionalidad,foto) -> None:
         self.id_interprete = id_interprete
-        self.nombre_interprete = nombre_interprete
-        self.apellido_interprete = apellido_interprete
+        self.nombre = nombre
+        self.apellido = apellido
         self.nacionalidad = nacionalidad
         self.foto = foto
 
-    def getId_interprete(self):
+    def getId_Interprete(self):
         return self.id_interprete
-    def getNombre_int(self):
-        return self.nombre_interprete
-    def getApellido_int(self):
-        return self.apellido_interprete
+    def getNombre(self):
+        return self.nombre
+    def getApellido(self):
+        return self.apellido
     def getNacionalidad(self):
         return self.nacionalidad
     def getFoto(self):
         return self.foto
 
-    def setId_interprete(self,idInterprete):
+    def setId_Interprete(self,idInterprete):
         self.id_interprete = idInterprete
-    def setNombre_int(self,nombre_interprete):
-        self.nombre_interprete = nombre_interprete
-    def setApellido_int(self,apellido_interprete):
-        self.apellido_interprete = apellido_interprete
+    def setNombre(self,nombre):
+        self.nombre = nombre
+    def setApellido(self,apellido):
+        self.apellido = apellido
     def setNacionalidad(self,nacionalidad):
         self.nacionalidad = nacionalidad
     def setFoto(self,foto):
         self.foto = foto
 
     def __str__(self) -> str:
-        return str(self.id_interprete)+' '+self.nombre_interprete+' '+self.apellido_interprete+' '+self.nacionalidad+' '+self.foto
+        return str(self.id_interprete)+' '+self.nombre+' '+self.apellido+' '+self.nacionalidad+' '+self.foto
 
 #---------------------------------------------------------------------------------------
 
 class Genero():
-    def __init__(self,id_genero,nombre_gen) -> None:
+    def __init__(self,id_genero,nombre) -> None:
         self.id_genero = id_genero
-        self.nombre_gen = nombre_gen
+        self.nombre = nombre
 
     def __str__(self) -> str:
-        return str(self.id_genero)+' '+self.nombre_gen
+        return str(self.id_genero)+' '+self.nombre
 
     def getId_genero(self):
         return self.getId_genero
-    def getNombre_gen(self):
-        return self.nombre_gen
+    def getNombre(self):
+        return self.nombre
 
     def setId_genero(self,id_genero):
         self.id_genero = id_genero
-    def setNombre_gen(self,nombre_gen):
-        self.nombre_gen = nombre_gen
+    def setNombre(self,nombre):
+        self.nombre = nombre
 
 
 #---------------------------------------------------------------------------------------
 
 class Discografica():
-    def __init__(self,id_discografica,nombre_disc) -> None:
+    def __init__(self,id_discografica,nombre) -> None:
         self.id_discografica = id_discografica
-        self.nombre_disc = nombre_disc
+        self.nombre = nombre
 
     def __str__(self) -> str:
-        return str(self.id_discografica)+' '+self.nombre_disc
+        return str(self.id_discografica)+' '+self.nombre
 
     def getId_discografica(self):
         return self.id_discografica
-    def getNombre_disc(self):
-        return self.nombre_disc
+    def getNombre(self):
+        return self.nombre
     
     def setId_discografica(self,id_discografica):
         self.id_discografica = id_discografica
-    def setNombre_disc(self,nombre_disc):
-        self.nombre_disc = nombre_disc
+    def setNombre(self,nombre):
+        self.nombre = nombre
 
 
 #---------------------------------------------------------------------------------------
 
 class Formato():
-    def __init__(self,id_formato,tipo_formato) -> None:
+    def __init__(self,id_formato,tipo) -> None:
         self.id_formato = id_formato
-        self.tipo_formato = tipo_formato
+        self.tipo = tipo
 
     def __str__(self) -> str:
-        return str(self.id_formato)+' '+self.tipo_formato
+        return str(self.id_formato)+' '+self.tipo
 
     def getId_formato(self):
         return self.id_formato
-    def getTipo_formato(self):
-        return self.tipo_formato
+    def getTipo(self):
+        return self.tipo
 
     def setId_formato(self,id_formato):
         self.id_formato = id_formato
-    def setTipo_formato(self,tipo_formato):
-        self.tipo_formato = tipo_formato
+    def setTipo(self,tipo):
+        self.tipo = tipo
 
 #---------------------------------------------------------------------------------------
-
 
 class Tema():
-    def __init__(self, id_tema, nombre_tema, autor, compositor) -> None:
+    def __init__(self,id_tema,titulo,duracion,autor,compositor,cod_album,id_interprete) -> None:
         self.id_tema = id_tema
-        self.nombre_tema = nombre_tema
-        self.autor = autor
-        self.compositor = compositor
-
-    def getId_tema(self):
-        return self.id_tema
-
-    def getNombre_tema(self):
-        return self.nombre_tema
-
-    def getAutor(self):
-        return self.autor
-
-    def getCompositor(self):
-        return self.compositor
-
-    def setId_tema(self, id_tema):
-        self.id_tema = id_tema
-
-    def setNombre_tema(self, nombre_tema):
-        self.nombre_tema = nombre_tema
-
-    def setAutor(self, autor):
-        self.autor = autor
-
-    def setCompositor(self, compositor):
-        self.compositor = compositor
-
-    def __str__(self) -> str:
-        return str(self.id_tema)+' '+self.nombre_tema+' '+self.autor+' '+self.compositor
-
-#---------------------------------------------------------------------------------------
-
-
-class Album_Tema():
-    def __init__(self, cod_album, id_tema) -> None:
-        self.cod_album = cod_album
-        self.id_tema = id_tema
-
-    def getId_tema(self):
-        return self.id_tema
-
-    def getCod_album(self):
-        return self.cod_album
-
-    def setId_tema(self, id_tema):
-        self.id_tema = id_tema
-
-    def setCod_album(self, cod_album):
-        self.cod_album = cod_album
-
-    def __str__(self) -> str:
-        return str(self.id_tema)+' '+str(self.cod_album)
-
-#---------------------------------------------------------------------------------------
-
-
-class Interprete_Tema():
-    def __init__(self, duracion, id_interprete, id_tema) -> None:
+        self.titulo = titulo
         self.duracion = duracion
+        self.autor = autor
+        self.compositor = compositor
+        self.cod_album = cod_album
         self.id_interprete = id_interprete
-        self.id_tema = id_tema
 
     def getId_tema(self):
         return self.id_tema
-
+    def getTiulo(self):
+        return self.titulo
     def getDuracion(self):
         return self.duracion
-
+    def getAutor(self):
+        return self.autor
+    def getCompositor(self):
+        return self.compositor
+    def getCod_album(self):
+        return self.cod_album
     def getId_interprete(self):
         return self.id_interprete
 
-    def setId_tema(self, id_tema):
+    def setId_tema(self,id_tema):
         self.id_tema = id_tema
-
-    def setDuracion(self, duracion):
+    def setTitulo(self,titulo):
+        self.titulo = titulo
+    def setDuracion(self,duracion):
         self.duracion = duracion
-
-    def setId_interprete(self, id_interprete):
+    def setAutor(self,autor):
+        self.autor = autor
+    def setCompositor(self,compositor):
+        self.compositor = compositor
+    def setCod_album(self,cod_album):
+        self.cod_album = cod_album
+    def setId_interprete(self,id_interprete):
         self.id_interprete = id_interprete
 
     def __str__(self) -> str:
-        return str(self.id_tema)+' '+str(self.duracion)+' '+str(self.id_interprete)
-
+        return str(self.id_tema)+' '+self.titulo+' '+str(self.duracion)+' '+self.autor+' '+self.compositor+' '+str(self.cod_album)+' '+str(self.id_interprete)
+    
 
 
 #---------------------------------------------------------------------------------------
 
 class Album():
-    def __init__(self,cod_album,nombre_album,id_genero,cant_temas,id_discografica,fec_lanzamiento,precio,cantidad,caratula) -> None:
+    def __init__(self,id_album,cod_album,nombre,id_interprete,id_genero,cant_temas,id_discografica,id_formato,fec_lanzamiento,precio,cantidad,caratula) -> None:
+        self.id_album = id_album
         self.cod_album = cod_album
-        self.nombre_album = nombre_album
+        self.nombre = nombre
+        self.id_interprete = id_interprete
         self.id_genero = id_genero
         self.cant_temas = cant_temas
         self.id_discografica = id_discografica
+        self.id_formato = id_formato
         self.fec_lanzamiento = fec_lanzamiento
         self.precio = precio
         self.cantidad = cantidad
         self.caratula = caratula
 
+    def getId_album(self):
+        return self.id_album
     def getCod_album(self):
         return self.cod_album
     def getNombre(self):
-        return self.nombre_album
+        return self.nombre
+    def getId_interprete(self):
+        return self.id_interprete
     def getId_genero(self):
         return self.id_genero
     def getCant_temas(self):
         return self.cant_temas
     def getId_discografica(self):
         return self.id_discografica
+    def getId_formato(self):
+        return self.id_formato
     def getFec_lanzamiento(self):
         return self.fec_lanzamiento
     def getPrecio(self):
@@ -386,16 +350,22 @@ class Album():
     def getCaratula(self):
         return self.caratula
 
+    def setId_album(self,id_album):
+        self.id_album = id_album
     def setCod_album(self,cod_album):
         self.cod_album = cod_album
-    def setNombre_album(self,nombre_album):
-        self.nombre_album = nombre_album
+    def setNombre(self,nombre):
+        self.nombre = nombre
+    def setId_interprete(self,id_interprete):
+        self.id_interprete = id_interprete
     def setId_genero(self,id_genero):
         self.id_genero = id_genero
     def setCant_temas(self,cant_temas):
         self.cant_temas = cant_temas
     def setId_discografica(self,id_discografica):
         self.id_discografica = id_discografica
+    def setId_formato(self,id_formato):
+        self.id_formato = id_formato
     def setFec_lanzamiento(self,fec_lanzamiento):
         self.fec_lanzamiento = fec_lanzamiento
     def setPrecio(self,precio):
@@ -406,56 +376,13 @@ class Album():
         self.caratula = caratula
 
     def __str__(self) -> str:
-        return str(self.cod_album) + ' ' + self.nombre_album + ' ' + str(self.id_genero) + ' ' + str(self.cant_temas) + ' ' + str(self.id_discografica) + ' ' + self.fec_lanzamiento + ' ' + str(self.precio) + ' ' + str(self.cantidad) + ' ' + self.caratula
+        return str(self.id_album) +' '+ str(self.cod_album) +' '+ self.nombre +' '+ str(self.id_interprete) +' '+ str(self.id_genero) +' '+ str(self.cant_temas) +' '+ str(self.id_discografica) +' '+ str(self.id_formato) +' '+ self.fec_lanzamiento +' '+ str(self.precio) +' '+ str(self.cantidad) +' '+ self.caratula
 
-    #---------------------------------------------------------------------------------------
+    
 
-
-class Album_Formato():
-    def __init__(self, id_formato, cod_album) -> None:
-        self.cod_album = cod_album
-        self.id_formato = id_formato
-
-    def getId_formato(self):
-        return self.id_formato
-
-    def getCod_album(self):
-        return self.cod_album
-
-    def setId_formato(self, id_formato):
-        self.id_formato = id_formato
-
-    def setCod_album(self, cod_album):
-        self.cod_album = cod_album
-
-    def __str__(self) -> str:
-        return str(self.id_formato)+' '+str(self.cod_album)
-#---------------------------------------------------------------------------------------
-
-
-class Album_Interprete():
-    def __init__(self, cod_album, id_interprete) -> None:
-        self.cod_album = cod_album
-        self.id_interprete = id_interprete
-
-    def getId_interprete(self):
-        return self.id_interprete
-
-    def getCod_album(self):
-        return self.cod_album
-
-    def setId_interprete(self, id_interprete):
-        self.id_interprete = id_interprete
-
-    def setCod_album(self, cod_album):
-        self.cod_album = cod_album
-
-    def __str__(self) -> str:
-        return str(self.cod_album)+' '+str(self.id_interprete)
     #---------------------------------------------------------------------------------------------------------
 
-#AComoAmor = Album(0,456783,'A Como Amor',3,5,10,5,3,'1978-01-01',899.99,3,'')
+AComoAmor = Album(0,456783,'A Como Amor',3,5,10,5,3,'1978-01-01',899.99,3,'')
 
-con = Conectar()
+#con = Conectar()
 #con.InsertarAlbum(AComoAmor)
-print(con.ListarInterprete())

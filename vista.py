22132633b5b69d0d15c0e562a5fc9d2330a12371
Gustavo Sarapura <gustavo.sarapura7@gmail.com>
2022-10-25 11:23:05 -0300

    Agregar cuadros a menu principal

--- vista.py
+++ vista.py
@@ -5,7 +5,7 @@ from rich.console  import Console
 from rich.progress import track
 from time import sleep 
 
-console = Console(width=80)
+console = Console(width=100)
 
 def clear():
     try:
@@ -73,23 +73,43 @@ incorrecto = 0
 while True:
     clear()
 
+    from rich import box
+    from rich.table import Table
+    from rich.align import Align
+
     console.rule("", style="bold gold3")
     console.print("DISQUERÍA FORMOSA MUSICAL", justify="center", style="bold white on deep_sky_blue4")
     console.rule("", style="bold gold3")
-    console.print("MENÚ PRINCIPAL\n", style="bold dark_orange3", justify="center")
-    console.print("1 - [bold]ALTA, BAJA O MODIFICACIÓN DE UN ÁLBUM[/]")
-    console.print("2 - [bold]LISTADO DE ÁLBUMES POR ARTISTAS[/]")
-    console.print("3 - [bold]LISTADO DE ÁLBUMES POR GÉNERO MUSICAL[/]")
-    console.print("4 - [bold]BÚSQUEDA POR NOMBRE DE ÁLBUM[/]")
-    console.print("5 - [bold]INSERTAR INTERPRETE[/]") # EXTRA
-    console.print("6 - [bold italic red]SALIR[/]\n")    
+
+    tabla_principal = Table(box=box.ROUNDED, show_header=False, style="bold gold3")
+    tabla_principal.add_row("[bold gold3]MENÚ PRINCIPAL")
+    console.print(Align.center(tabla_principal))
+
+    tabla_opcion = Table(expand=True, style="deep_sky_blue3", box=box.DOUBLE_EDGE, show_header=False, highlight=True)
+    tabla_opcion.add_row("[cyan bold]1[/] - [bold]ALTA, BAJA O MODIFICACIÓN DE UN ÁLBUM[/]")
+    tabla_opcion.add_row("[cyan bold]2[/] - [bold]LISTADO DE ÁLBUMES POR ARTISTAS[/]")
+    tabla_opcion.add_row("[cyan bold]3[/] - [bold]LISTADO DE ÁLBUMES POR GÉNERO MUSICAL[/]")
+    tabla_opcion.add_row("[cyan bold]4[/] - [bold]BÚSQUEDA POR NOMBRE DE ÁLBUM[/]")
+    tabla_opcion.add_row("[cyan bold]5[/] - [bold]INSERTAR INTERPRETE[/]") # EXTRA
+    tabla_opcion.add_row("[cyan bold]6[/] - [bold italic red1]SALIR[/]") 
+    console.print(tabla_opcion)
 
     try:
         if incorrecto == 1:
-            console.print("[i]¡Opción [bold red]incorrecta[/]![/i] :flushed:")
+            tabla_incorrecto = Table(expand=True, style="red1", box=box.ASCII, show_header=False, highlight=True)
+            tabla_incorrecto.add_row("[i]¡Opción [bold red1]incorrecta[/]![/i] :flushed:")
+            console.print(tabla_incorrecto)
+
         if valuerror == 1:
-            console.print("[i]Ingrese solo [bold gold3]números[/], por favor[/i] :confused:.")
-        opcion = int(console.input("[i]Ingrese su [bold cyan]opción[/][/i] :smiley:: ")) 
+            tabla_error = Table(expand=True, style="dark_orange3", box=box.ASCII, show_header=False, highlight=True)
+            tabla_error.add_row("[i]Ingrese solo [bold dark_orange3]números[/], por favor[/i] :confused:.")
+            console.print(tabla_error)
+        
+        tabla_op = Table(expand=True, style="cyan", box=box.ASCII2, show_header=False, highlight=True)
+        tabla_op.add_row("[i]Ingrese su [bold cyan]opción[/][/i] :smiley:: ")
+        console.print(tabla_op)
+        opcion = int(console.input("[bold cyan]>: "))
+
         assert opcion <= 6
     except ValueError:
         valuerror = 1

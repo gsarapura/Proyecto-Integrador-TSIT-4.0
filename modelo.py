d4fda277efc167a444e73d9fe27415dcc5a99943
HernanCastle <hernan.quintana@mi.unc.edu.ar>
2022-10-28 21:37:57 -0300

    Cambios Select busque bien los vigentes

--- modelo.py
+++ modelo.py
@@ -46,7 +46,7 @@ class Conectar():
         if self.conexion.is_connected():
             try:
                 cursor = self.conexion.cursor()
-                senteciaSQL = "SELECT genero.nombre,cod_album, album.nombre, interprete.apellido, interprete.nombre, discografica.nombre, precio, cantidad, formato.tipo FROM album INNER JOIN interprete ON album.id_interprete = interprete.id_interprete INNER JOIN discografica ON album.id_discografica = discografica.id_discografica INNER JOIN formato ON album.id_formato = formato.id_formato INNER JOIN genero ON album.id_genero = genero.id_genero WHERE vigente = 1  ORDER By genero.nombre asc, interprete.apellido, interprete.nombre"
+                senteciaSQL = "SELECT genero.nombre,cod_album, album.nombre, interprete.apellido, interprete.nombre, discografica.nombre, precio, cantidad, formato.tipo FROM album INNER JOIN interprete ON album.id_interprete = interprete.id_interprete INNER JOIN discografica ON album.id_discografica = discografica.id_discografica INNER JOIN formato ON album.id_formato = formato.id_formato INNER JOIN genero ON album.id_genero = genero.id_genero WHERE genero.vigente = 1  ORDER By genero.nombre asc, interprete.apellido, interprete.nombre"
                 # Código Hernán:
                 # senteciaSQL = "SELECT cod_album, album.nombre, interprete.nombre, interprete.apellido, genero.nombre, discografica.nombre, precio, cantidad, formato.tipo FROM album, interprete, discografica,formato,genero WHERE album.id_interprete = interprete.id_interprete AND album.id_discografica = discografica.id_discografica AND album.id_formato = formato.id_formato AND album.id_genero = genero.id_genero and album.vigente = 1 ORDER By genero.nombre asc"
                 # el vigente = 1 es para que no me traiga los que estan eliminados
@@ -73,7 +73,7 @@ class Conectar():
         if self.conexion.is_connected():
             try:
                 cursor = self.conexion.cursor()
-                revisoExistenciaSQL = "SELECT * from interprete where nombre = %s and apellido = %s"
+                revisoExistenciaSQL = "SELECT * from interprete where nombre = %s and apellido = %s "
                 data = (interprete.getNombre(),interprete.getApellido())
                 cursor.execute(revisoExistenciaSQL,data)
                 resultado = cursor.fetchall()
